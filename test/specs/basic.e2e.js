71134109782b8c4699ade0e1fb7c2c04f626ab30
hesoyam124 <sale124@live.com>
2021-12-09 19:39:20 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,12 +1,12 @@
 describe('shoutem pricing lista', () => {
     it('postoji li h1 element', async () => {
-       await browser.url('https://shoutem.com/pricing/');
-       
-       const h1 = $('h1') ;
+        await browser.url('https://shoutem.com/pricing/');
 
-       await expect(h1).toHaveText('There are more ways to create apps with Shoutem');
+        const h1 = $('h1');
 
- 
+        await expect(h1).toHaveText('There are more ways to create apps with Shoutem');
+
+
 
 
     })
@@ -17,14 +17,14 @@ describe('button', () => {
     it('je li klikabilan i otvara li se nova stranica heheheheheheheh', async () => {
 
 
-       await browser.url('https://shoutem.com/apps/social-network/');
+        await browser.url('https://shoutem.com/apps/social-network/');
 
-       const buildnow = await $('a=Build now');
-       await buildnow.click();
+        const buildnow = await $('a=Build now');
+        await buildnow.click();
 
-       const link = await $('.page-title');
+        const link = await $('.page-title');
 
-       await expect(link).toHaveText('Choose an app template');
+        await expect(link).toHaveText('Choose an app template');
 
 
 
