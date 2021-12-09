c7976069b435146e5d03d70e24f742bfcdf155d0
mpavic2 <mpavic2@ffos.hr>
2021-12-09 19:50:22 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,21 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+describe('Shoutem Web stranica', () => {
+    it('Ucitavanje pocetne stranice', async () => {
+       await browser.url('https://shoutem.com/pricing/');
+       const h1 = await $('h1');
+       await expect(h1).toHaveText('There are more ways to create apps with Shoutem');
+
+      
     })
+
+    it('Otvaranje Social Network landing stranice', async () => {
+        await browser.url('https://shoutem.com/apps/social-network/');
+        const button = await $('.btn=Build now');
+        await button.click();
+        const header = await $('h1');
+        await expect(header).toHaveText('Choose an app template');
+        
+        
+
+    })
+
 })
