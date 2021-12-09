a020904d13f279e530ea02a8596f6fcd73e33732
Eva Stojić <eva.k.stojic@gmail.com>
2021-12-09 19:26:39 +0100

    new tests added

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,17 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+describe('Open Shoutem Website', () => {
+    it('Proper title is displayed', async () => {
+        await browser.url('https://shoutem.com/pricing/');
+
+        const title = await $('h1');
+
+        await expect(title).toHaveText('There are more ways to create apps with Shoutem');
+    })
+
+    it('Ssocial Network', async () => {
+        await browser.url('https://shoutem.com/apps/social-network/');
+        const myButton = await $('a=Build now');
+        await myButton.click();
+        const title = await $('h1');
+        await expect(title).toHaveText('Choose an app template');
     })
 })
