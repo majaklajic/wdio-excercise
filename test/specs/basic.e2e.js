3e665ccbd1d275613298d8035f28107dbbd8084a
ibilonic4 <ibilonic22@gmail.com>
2021-12-09 19:36:31 +0100

    fasf
    fsaf

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -5,3 +5,17 @@ describe('Shoutem website', () => {
       await  expect(hed).toHaveText('There are more ways to create apps with Shoutem');
     })
 })
+
+
+describe('Shoutem buttoni', () => {
+it('Shoutem media page', async () => {
+
+await browser.url('https://shoutem.com/apps/social-network/');
+const btn = await $('a=Build now');
+await btn.click();
+const temp = await $('.page-title');
+await expect(temp).toHaveText('Choose an app template');
+
+})
+
+})
