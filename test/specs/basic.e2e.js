7f4f11e79d7a014b95d6760a90757e03e308a922
danka10121012 <91868920+danka10121012@users.noreply.github.com>
2021-12-16 19:38:14 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,50 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+describe ('OpenMRS', () => {
+    it ('Login with incorrect credentials', async () => {
+    browser.url("https://demo.openmrs.org/openmrs/login.htm");
+    
+    const usernameInput = await $('#username');
+    const passwordInput = await $('#password');
+    const pharmacyButton = await $('#Pharmacy');
+    const loginButton = await $('#loginButton');
+    
+    await usernameInput.addValue('tre3');
+    await passwordInput.addValue('t3t43wzks');
+    await pharmacyButton.click();
+    await loginButton.click();
+    
+    const errorMessage = await $('#error-message');
+    await expect(errorMessage).toHaveText("Invalid username/password. Please try again.");
+    
+    
+    });
+    
+    it ('Login with correct credentials', async () => {
+    browser.url("https://demo.openmrs.org/openmrs/login.htm");
+    
+    const usernameInput = await $('#username');
+    const passwordInput = await $('#password');
+    const pharmacyButton = await $('#Pharmacy');
+    const loginButton = await $('#loginButton');
+    
+    await usernameInput.addValue('Admin');
+    await passwordInput.addValue('Admin123');
+    await pharmacyButton.click();
+    await loginButton.click();
+    
+    const h4 = await $('h4');
+    await expect(h4).toHaveText("Logged in as Super User (admin) at Pharmacy.");
+    });
+    
+    it ('Logout', async () => {
+    browser.setWindowSize(1500, 1500);
+    
+    const LogoutButton = await $('a=Logout');
+    await LogoutButton.click();
+    
+    const Login = await $('.w-auto');
+    await expect(Login).toHaveText("LOGIN");
+    
+    
+    });
+    
     })
-})
