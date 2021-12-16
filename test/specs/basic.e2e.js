a6998e372fea8736b40b0bbc86ab164518937cf8
dhabek <dunja.habek@hotmail.com>
2021-12-16 19:39:27 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
-    })
-})
-=======
-// describe ("Shoutem pricing stranica", () => {
-//     it('Testiranje h1', async () => {
-//         await browser.url('https://shoutem.com/pricing/');
-//         const h1 = await $('h1');
-//         await expect(h1).toHaveText('There are more ways to create apps with Shoutem');
-
-//     })
-
-//     it ('Otvaranje Social Network i klikanje gumba', async() => {
-//         await browser.url('https://shoutem.com/apps/social-network/');
-//         const button = await $('.btn=Build now');
-//         await button.click();
-//         const header = await $('h1');
-//         await expect(header).toHaveText('Choose an app template');
-//     })
-//})
 
 describe ("Open MRS website", () => {
     it ('Login with incorrect credentials', async () => { await browser.url("https://demo.openmrs.org/openmrs/login.htm");
@@ -78,4 +54,3 @@ describe ("Open MRS website", () => {
     })
 
 })
->>>>>>> Stashed changes
