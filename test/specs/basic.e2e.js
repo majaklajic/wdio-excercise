f7dfa7b5f48bcad324a244b544f0aa807526a4f3
kcukac07 <115490691+kcukac07@users.noreply.github.com>
2022-12-06 18:55:42 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,25 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
-    })
-})
+describe("Shoutem web", async () => {
+ /**    it("Check if Shoutem Pricing page is loading", async () => {
+        await browser.url("https://shoutem.com/pricing/");
+
+        const heading = await $("h1");
+        await expect(heading).toHaveText("There are more ways to create apps with Shoutem");
+    });
+*/
+
+    it("Test Social template page", async () => {
+        await browser.url("https://shoutem.com/apps/social-network/");
+
+        const buildNowButton = await $("a=Build now"); //prvo nađem preko inspekta preko teksta na gumbu
+        
+        await buildNowButton.click(); //kliknem na gumb
+
+        await browser.pause(5000); //pauza da vidim jesam sve dobro napravila dosad
+
+      /**   const heading = await $("h1");
+        await expect(heading).toHaveText("Choose an app template");  ILI */
+
+        const heading = await $(".page-title");
+        await expect(heading).toHaveText("Choose an app template");
+    });
+});
