28901e4326ef1146ddd41e641fa9cae011e6c76a
kmataija <kmataija@ffos.hr>
2022-12-06 18:52:27 +0100

    WebdriverIO

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,22 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
-    })
-})
+describe("Shoutem web", async () => {
+
+ /**   it("Check if Shoutem Pricing page is loading", async () =>{
+        await browser.url("https://shoutem.com/pricing/");
+        await browser.pause(5000);
+
+        const heading = await $("h1");
+        await expect(heading).toHaveText("There are more ways to create apps with Shoutem");
+
+    });
+*/ 
+    it("Test Social template page", async () =>{
+        await browser.url("https://shoutem.com/apps/social-network/");
+        const btn = await $('a=BUILD NOW');
+        await btn.click();
+
+        const heading = await $("h1");
+        await expect(heading).toHaveText("Choose an app template");
+
+    });
+
+});
