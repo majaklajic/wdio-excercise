8009612b0edaeca7b3f5903e0bbff9871fa15b85
Ena Maurus <emaurus@ffos.hr>
2022-12-13 18:33:14 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,75 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
-    })
-})
+/* 
+describe("Swag Labs", async () => {
+
+    it("Check if Login/Logout works", async () => {
+        await browser.url("https://www.saucedemo.com/");
+
+        const username = await $("#user-name");
+        await username.click();
+        await browser.keys(['v','a','v','z',]);
+
+        const password = await $("#password");
+        await password.click();
+        await browser.keys(['v','3','v','z','z']);
+
+        const button = await $("#login-button");
+        await button.click();
+       
+        const error = await $("h3");
+        await expect(error).toHaveText("Epic sadface: Username and password do not match any user in this service");
+    });
+
+});
+
+
+describe("Swag Labs", async () => {
+
+    it("Check if Login/Logout works", async () => {
+        await browser.url("https://www.saucedemo.com/");
+
+        const username = await $("#user-name");
+        await username.click();
+        await browser.keys(['s','t','a','n','d','a','r','d','_','u','s','e','r']);
+
+        const password = await $("#password");
+        await password.click();
+        await browser.keys(['s','e','c','r','e','t','_','s','a','u','c','e']);
+
+        const button = await $("#login-button");
+        await button.click();
+       
+        const inventor = await $("#inventory_container");
+        await expect(inventor).toBeDisplayed()
+    });
+
+});
+
+*/
+
+describe("Swag Labs", async () => {
+
+    it("Check if Login/Logout works", async () => {
+        await browser.url("https://www.saucedemo.com/");
+
+        const username = await $("#user-name");
+        await username.click();
+        await browser.keys(['s','t','a','n','d','a','r','d','_','u','s','e','r']);
+
+        const password = await $("#password");
+        await password.click();
+        await browser.keys(['s','e','c','r','e','t','_','s','a','u','c','e']);
+
+        const button = await $("#login-button");
+        await button.click();
+
+        const hamburger = await $(".bm-burger-button");
+        await hamburger.click();
+
+        const logout = await $("#logout_sidebar_link");
+        await logout.click();
+       
+        const login = await $(".login-box");
+        await expect(login).toBeDisplayed()
+    });
+
+});
